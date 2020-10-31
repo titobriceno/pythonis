95699619c35fe15fbf177cdbf8f577ca41347b51
Tito Oswaldo <titooswaldobriceno@gmail.com>
2020-10-30 23:33:21 -0500

    este es mi primer commit

---
+++ practica_git.py
@@ -0,0 +1,8 @@
+def run ():
+    print("este es mi primer programa para git")
+    print("en este narraremos nuestra esta historia en la programacion")
+    print("nuestro primer programa para el ejercicio de git ")
+
+
+if __name__ == "__main__":
+    run()
