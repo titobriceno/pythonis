d45a41354fc7be8db61a8021e7945ef43ee9fae7
tito oswaldo <titooswaldobriceno@gmail.com>
2020-10-31 23:24:57 -0500

    se cambia la descripcion del programa

--- practica_git.py
+++ practica_git.py
@@ -1,14 +1,14 @@
 def run ():
     print("este es mi primer programa para git")
     print("en este narraremos nuestra esta historia en la programacion")
-    print("nuestro primer programa para el ejercicio de git ")
+    print("este programa realiza suma de numeros ")
     funcion_suma()
 
 def funcion_suma ():
 
     print("por favor registra los valores para realizar la suma")
-    valor_1 = float(input("registra el primer valor"))
-    valor_2 = float(input("registra el segundo valor"))
+    valor_1 = float(input("registra el primer valor "))
+    valor_2 = float(input("registra el segundo valor "))
     suma = valor_1 + valor_2
 
     print(suma)
